800152bc442fc63af1f7ef50900d1052b05f81c1
Nirmal kumar Ravi <nravi@hawk.iit.edu>
2015-04-13 21:46:18 -0500

    s3 interface

--- thadoop/src/thadoop/s3/S3FileSystem.java
+++ thadoop/src/thadoop/s3/S3FileSystem.java
@@ -1,8 +1,17 @@
 package thadoop.s3;
+
+import java.io.File;
+import java.io.InputStream;
+
 /*
  * A thin Interface to s3 to get 
  * files and put files 
  */
 public class S3FileSystem {
-
+	public InputStream getFile(String fileName){
+		return null;
+	}
+	public boolean putFile(File file){
+		return false;
+	}
 }
