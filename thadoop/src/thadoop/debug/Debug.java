ce841deb969b7adefbcfaeb0aac6ce5c8984912b
Nirmal kumar Ravi <nravi@hawk.iit.edu>
2015-04-23 22:16:39 -0500

    corrected method name

--- thadoop/src/thadoop/debug/Debug.java
+++ thadoop/src/thadoop/debug/Debug.java
@@ -1,7 +1,7 @@
 package thadoop.debug;
 
 public class Debug {
-	public static void printDebubMessage(String message){
+	public static void printDebugMessage(String message){
 		System.out.println(message);
 	}
 	public static void printErrorMessage(String message){
