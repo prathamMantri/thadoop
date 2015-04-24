0bcedcc7cb760a0d8a0bea547dcced8325f325d0
Nirmal kumar Ravi <nravi@hawk.iit.edu>
2015-04-23 22:11:05 -0500

    new class for debugging

---
+++ thadoop/src/thadoop/debug/Debug.java
@@ -0,0 +1,16 @@
+package thadoop.debug;
+
+public class Debug {
+	public static void printDebubMessage(String message){
+		System.out.println(message);
+	}
+	public static void printErrorMessage(String message){
+		System.out.println(message);
+	}
+	public static void printWarningMessage(String message){
+		System.out.println(message);
+	}
+	public static void printInfoMessage(String message){
+		System.out.println(message);
+	}
+}
