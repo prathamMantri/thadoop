9e3058f6e1eda4d2eeafede1280fa1d128326110
Nirmal kumar Ravi <nravi@hawk.iit.edu>
2015-04-26 18:17:24 -0500

    changed mapper and reducer signature

--- thadoop/src/thadoop/mapreduce/MapReduce.java
+++ thadoop/src/thadoop/mapreduce/MapReduce.java
@@ -8,18 +8,43 @@
 package thadoop.mapreduce;
 
 import java.io.Serializable;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+
+import thadoop.s3.FileMetaData;
 
 public class MapReduce implements Serializable
 {
 	private static final long serialVersionUID = -6867043043090117875L;
-	public String mapper(){
-		return "This is Mapper Method";
+	private Path inputPath;
+	private Path outputPath;
+	
+	public MapReduce(Path inputPath,Path outputPath){
+		this.inputPath = inputPath;
+		this.outputPath = outputPath;
 	}
-	public void reducer(){
-		
-		
+	
+	public Map<String,Integer> mapper(FileMetaData fileMetaData){
+		return null;
 	}
-	public void combiner(){
-		
+	public Map<String,Integer> reducer(List<FileMetaData> fileMetaDatas){
+		return null;
 	}
+	public Path getInputPath() {
+		return inputPath;
+	}
+
+	public void setInputPath(Path inputPath) {
+		this.inputPath = inputPath;
+	}
+
+	public Path getOutputPath() {
+		return outputPath;
+	}
+
+	public void setOutputPath(Path outputPath) {
+		this.outputPath = outputPath;
+	}
+	
 }
