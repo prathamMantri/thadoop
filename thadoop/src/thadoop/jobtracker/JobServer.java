cd791b37b9934a7184e1b51c62a874ad793525a5
Nirmal kumar Ravi <nravi@hawk.iit.edu>
2015-04-25 20:42:58 -0500

    checked in job tracker code

--- thadoop/src/thadoop/jobtracker/JobServer.java
+++ thadoop/src/thadoop/jobtracker/JobServer.java
@@ -4,13 +4,36 @@ import thadoop.debug.Debug;
 import thadoop.server.Server;
 public class JobServer{
 	
-	public boolean startJobServer(){
-		Debug.infoMessage("server Stopped");
-		return false;
+	private JobServerThread jobServerThread;
+	
+	public JobServer(int portNumber){
+		this.jobServerThread = new JobServerThread(portNumber);
 	}
-	public boolean stopJobServer(){
-		Debug.infoMessage("server Started");
-		return false;
-		
+	
+	public boolean startJobServer(){
+		Debug.infoMessage("starting job server");
+		if(jobServerThread.isAlive()){
+			Debug.infoMessage("job server is already running");
+			return false;
+		}
+		this.jobServerThread.start();
+		Debug.infoMessage("job server started");
+		return true;
+	}
+	public void stopServer(){
+		Debug.infoMessage("Stopping Server....");
+		if(!jobServerThread.isAlive()){
+			Debug.infoMessage("Server is already stopped ");
+			return;
+		}
+		this.jobServerThread.stopMe();
+	}
+	public void isServerAlive(){
+		if(this.jobServerThread.isAlive()){
+			Debug.infoMessage("Yes");
+			
+		}else{
+			Debug.infoMessage("No");
+		}
 	}
 }
