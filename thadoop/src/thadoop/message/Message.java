d6b6b18bab296d727775be8e0843cd0d57567ed8
Krishnaja <kethireddy.krishnaja@gmail.com>
2015-04-25 20:42:32 -0500

    Added serialVersionID

--- thadoop/src/thadoop/message/Message.java
+++ thadoop/src/thadoop/message/Message.java
@@ -3,5 +3,7 @@ package thadoop.message;
 import java.io.Serializable;
 
 public class Message implements Serializable{
+	
+	private static final long serialVersionUID = -6867043043090117875L;
 	public MessageID messageID;
 }
