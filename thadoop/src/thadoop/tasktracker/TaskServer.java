2b3bb2d3d71fef1c7bf8300209950eda7b09e67a
prathamMantri <pratham.mantri@gmail.com>
2015-04-25 22:28:50 -0500

    Code for Task Server

--- thadoop/src/thadoop/tasktracker/TaskServer.java
+++ thadoop/src/thadoop/tasktracker/TaskServer.java
@@ -1,7 +1,43 @@
 package thadoop.tasktracker;
 
+import thadoop.debug.Debug;
+import thadoop.jobtracker.JobServerThread;
 import thadoop.server.Server;
 
 public class TaskServer{
+	
+private TaskServerThread jobServerThread;
+	
+	public TaskServer(int portNumber){
+		this.jobServerThread = new TaskServerThread(portNumber);
+	}
+	
+	public boolean startJobServer(){
+		Debug.infoMessage("starting job server");
+		if(jobServerThread.isAlive()){
+			Debug.infoMessage("job server is already running");
+			return false;
+		}
+		this.jobServerThread.start();
+		Debug.infoMessage("job server started");
+		return true;
+	}
+	public void stopServer(){
+		Debug.infoMessage("Stopping Server....");
+		if(!jobServerThread.isAlive()){
+			Debug.infoMessage("Server is already stopped ");
+			return;
+		}
+		this.jobServerThread.stopMe();
+	}
+	public void isServerAlive(){
+		if(this.jobServerThread.isAlive()){
+			Debug.infoMessage("Yes");
+			
+		}else{
+			Debug.infoMessage("No");
+		}
+	}
+	
 	
 }
