7158d6e284fc88d8bb6197c005e04195c7154161
Nirmal kumar Ravi <nravi@hawk.iit.edu>
2015-04-27 17:37:21 -0500

    added two new messages

--- thadoop/src/thadoop/message/MessageID.java
+++ thadoop/src/thadoop/message/MessageID.java
@@ -2,6 +2,8 @@ package thadoop.message;
 
 public enum MessageID {
 	MAPPER_JOB,
+	MAPPER_JOB_REPLY,//reply after completion of mappers from task tracker
 	REDUCER_JOB,
+	REDUCER_JOB_REPLY,//reply after completion of reducers from task tracker
 	MAPREDUCE_JOB //To do Map, Reduce,Combine job
 }
