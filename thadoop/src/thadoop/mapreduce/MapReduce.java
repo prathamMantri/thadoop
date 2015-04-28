defe0eb7894b440f339fdda2020bee04e6ccb84a
Nirmal kumar Ravi <nravi@hawk.iit.edu>
2015-04-27 21:51:50 -0500

    map reduce code

--- thadoop/src/thadoop/mapreduce/MapReduce.java
+++ thadoop/src/thadoop/mapreduce/MapReduce.java
@@ -1,19 +1,13 @@
-/**
- * 
- */
-/**
- * @author Krishnaja
- *
- */
 package thadoop.mapreduce;
 
 import java.io.Serializable;
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import thadoop.s3.FileMetaData;
-
+/*
+ * contains word count program 
+ */
 public class MapReduce implements Serializable
 {
 	private static final long serialVersionUID = -6867043043090117875L;
@@ -25,11 +19,30 @@ public class MapReduce implements Serializable
 		this.outputPath = outputPath;
 	}
 	
-	public Map<String,Integer> mapper(FileMetaData fileMetaData){
-		return null;
+	public Map<String,Integer> mapper(Map<Integer,String> input){
+		Map<String,Integer> output = new HashMap<String,Integer>();
+		for(Integer lineNum: input.keySet()){
+			String line = input.get(lineNum);
+			for(String word: line.split("\\s+")){
+				Integer val = output.get(word);
+				if(val == null)
+					val = 0;
+				val = val + 1;
+				output.put(word, val);
+			}
+		}
+		return output;
 	}
-	public Map<String,Integer> reducer(List<FileMetaData> fileMetaDatas){
-		return null;
+	public Map<String,Integer> reducer(Map<String,List<Integer>> input){
+		Map<String,Integer> output = new HashMap<String,Integer>();
+		for(String word: input.keySet()){
+			Integer sum = 0;
+			for(Integer val : input.get(word)){
+				sum = sum + val;
+			}
+			output.put(word, sum);
+		}
+		return output;
 	}
 	public Path getInputPath() {
 		return inputPath;
